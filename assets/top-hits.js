4ec664f298d17d8831a4ebe70ffe0df2bb74d77c
Nevan <nev.kd2832@gmail.com>
2022-02-02 12:32:50 -0500

    created functional HTML, CSS and JS files to correctly extract song info using YouTube API and display it on the website.

---
+++ assets/top-hits.js
@@ -0,0 +1,69 @@
+//  Declarations
+var songSearch = $("#song").val();
+var requestUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet";
+var testRequest = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=theweeknd&key=AIzaSyCRZy3dTHCrHziFGy-k-XHY9AkNrvd8a_k";
+let APIkey = 'AIzaSyCRZy3dTHCrHziFGy-k-XHY9AkNrvd8a_k';
+var video = '';
+var videos = $()
+
+// Functions
+// Search Manipulation Function
+function refinedSearch(search){
+    let searchList = search.split(' ');
+    console.log(searchList); 
+    let song = '';
+    for (let i = 0; i<searchList.length; i++){
+        song+=searchList[i];
+    }
+    console.log(song);
+
+    let youtubeRequestUrl = requestUrl + '&q=' + song + '&key=' + APIkey;
+    console.log(youtubeRequestUrl);
+
+    youtubeSearch(youtubeRequestUrl);
+}
+
+// JSON parsing, extracting YouTube thumbnail URL
+function youtubeSearch(link){
+    fetch(link)
+    .then(function (response) {
+        return(response.json());
+    })
+    .then(function (data) {
+        // console.log(data.value.items[0].);
+        console.log(data);
+        let video= data.items[0];
+        console.log(video);
+
+        let videoID = video.id.videoId
+        console.log(videoID);
+
+        displayVideo(video, videoID);
+        
+    });
+}
+
+// video display code
+/* <iframe width="420" height="315" src="http://www.youtube.com/embed/watch?v=${video.id.videoId}" frameborder="0" allowfullscreen></iframe> */
+
+/* <a href="https://www.youtube.com/embed/${videoID}"><img src= "${video.snippet.thumbnails.medium.url}" height="315" width="420"></img></a>
+<figcaption>${video.snippet.title}</figcaption> */
+
+
+// Display Video Function
+function displayVideo(video, videoID){
+    let videoContainer = document.querySelector(".youtube-video-container")
+        videoContainer.innerHTML += `
+
+        <div class="video-display">
+        <a href="https://www.youtube.com/watch?v=${videoID}"><img src= "${video.snippet.thumbnails.medium.url}" height="315" width="420"></img></a>
+        <figcaption>${video.snippet.title}</figcaption>
+        </div>
+        `;
+}
+
+
+
+// Calling
+refinedSearch(songSearch);
+// youtubeSearch(testRequest);
